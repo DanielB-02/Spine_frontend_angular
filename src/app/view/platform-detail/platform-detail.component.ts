fc95932751123390ac7b79234260f4c5f668ffb9
s1145204 <1145204@student.hsleiden.nl>
2023-12-10 14:07:39 +0100

    feat added answers to questions     also put these in inputs, so disabling them for readonly shouldnt be too difficult

--- src/app/view/platform-detail/platform-detail.component.ts
+++ src/app/view/platform-detail/platform-detail.component.ts
@@ -4,6 +4,11 @@ import {Question} from "../../model/question/question";
 import {QuestionService} from "../../services/question.service";
 import {Platform} from "../../model/platform/platform";
 import {PlatformService} from "../../services/platform.service";
+import {Answer} from "../../model/answer/answer";
+import {AnswerService} from "../../services/answer.service";
+import { FormsModule } from '@angular/forms';
+import { forkJoin } from 'rxjs';
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-platform-detail',
@@ -12,18 +17,23 @@ import {PlatformService} from "../../services/platform.service";
 })
 export class PlatformDetailComponent implements OnInit {
   platformId: string;
+  questionId: string;
   platform: Platform;
   questions: Question[];
+  answers: Answer[];
 
   constructor(private route: ActivatedRoute,
+              private answerService: AnswerService,
               private questionService: QuestionService,
-              private platformService: PlatformService) { }
+              private platformService: PlatformService) {
+  }
 
   ngOnInit(): void {
     this.platformId = this.route.snapshot.paramMap.get('id');
     this.loadPlatform();
     this.loadQuestions();
   }
+
   private loadPlatform(): void {
     this.platformService.find(this.platformId)
       .subscribe(platform => this.platform = platform);
@@ -31,6 +41,26 @@ export class PlatformDetailComponent implements OnInit {
 
   private loadQuestions(): void {
     this.questionService.getQuestionsForPlatform(this.platformId)
-      .subscribe(questions => this.questions = questions);
+      .subscribe(questions => {
+        this.questions = questions;
+
+        // Load answers for each question
+        const answerObservables = this.questions.map(question => this.loadAnswer(question.id));
+
+        // Use forkJoin to wait for all answers to be loaded
+        forkJoin(answerObservables).subscribe(answersArray => {
+          // Assign answers to their respective questions
+          this.questions.forEach((question, index) => {
+            question.answers = answersArray[index];
+          });
+        });
+      });
+  }
+
+  private loadAnswer(questionId: string) {
+    return this.answerService.getAnswersOfQuestion(questionId).pipe(
+      // Use map to extract the first answer from the array
+      map(answers => answers.length > 0 ? [answers[0]] : [])
+    );
   }
 }
