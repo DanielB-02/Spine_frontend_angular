796951bcdb3c69fc6d8d6f1de7d8f1d8c672408c
DanielB-02 <daniel.d.bor@gmail.com>
2023-12-11 18:27:11 +0100

    Login Routes completed
    Session reset when logged out and styling applied.

--- src/app/view/navigation-column/navigation-column.component.ts
+++ src/app/view/navigation-column/navigation-column.component.ts
@@ -1,34 +1,23 @@
 import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
 
 import {UserStorageService} from "../../auth/user-storage.service";
-import {LoginStateService} from "../../services/login.state.service";
 import {Router} from "@angular/router";
-import {FormBuilder} from "@angular/forms";
 import {AuthService} from "../../auth/auth.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-navigation-column',
   templateUrl: './navigation-column.component.html',
   styleUrl: './navigation-column.component.scss'
 })
-export class NavigationColumnComponent implements AfterViewInit {
-  @ViewChild('uitloggenLink') uitloggenLink: ElementRef;
-
+export class NavigationColumnComponent {
     constructor(
       private router: Router,
       private authService: AuthService
   ) {}
 
-  ngAfterViewInit(): void  {
-    this.uitloggenLink.nativeElement.addEventListener('click', () => {
-
-      // this.authService.logout();
-    })
-  }
-
-  tets(): void {
+  uitloggenClicked(): void {
     // alert('tets');
     this.router.navigateByUrl('/login');
+    this.authService.logout()
   }
 }
